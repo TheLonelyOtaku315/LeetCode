536319c23cec566d1d30730a3e978c5182b8ba27
Tonny Zhao <94974464+TheLonelyOtaku315@users.noreply.github.com>
2025-01-17 13:07:48 -0500

    Time: 4 ms (2.52%), Space: 41.3 MB (98.54%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -2,7 +2,6 @@ class Solution {
     public int lengthOfLastWord(String s) {
         String regex = "[,\\s\\.]";
         String[] words = s.split(regex);
-
         return words[words.length - 1].length();
     }
 }

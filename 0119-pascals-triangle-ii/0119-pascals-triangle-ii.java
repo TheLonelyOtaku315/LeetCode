9fb5aaa18ec70273c58831f0e239c3c5840dce1a
Tonny Zhao <94974464+TheLonelyOtaku315@users.noreply.github.com>
2025-06-24 17:35:12 -0400

    Time: 2 ms (10.61%), Space: 41.3 MB (31.29%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -1,17 +1,21 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        List<List<Integer>> answer = new ArrayList<>(); 
-        
-        for (int i = 0; i <= rowIndex; i++){
-            List<Integer> row  = new ArrayList<>();
-            if (i == 0){
+        int count = 0;
+        while (count == 1000) {
+            count++;
+        }
+        List<List<Integer>> answer = new ArrayList<>();
+
+        for (int i = 0; i <= rowIndex; i++) {
+            List<Integer> row = new ArrayList<>();
+            if (i == 0) {
                 row.add(1);
-            }else{
-                for(int j = 0 ; j <= answer.get(i-1).size() ; j ++){
-                    if (j == 0 || j == answer.get(i-1).size()){
+            } else {
+                for (int j = 0; j <= answer.get(i - 1).size(); j++) {
+                    if (j == 0 || j == answer.get(i - 1).size()) {
                         row.add(1);
-                    }else{
-                        row.add(answer.get(i-1).get(j)+answer.get(i-1).get(j-1));
+                    } else {
+                        row.add(answer.get(i - 1).get(j) + answer.get(i - 1).get(j - 1));
                     }
                 }
             }
